7895615fb236384b24ba7d6f2305ba849d783e43
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-03-02 04:00:27 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -12,7 +12,7 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
 
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
+${user.banned ? 'kamu dibanned' : '_*Ada Yang Saya Bisa Ewe❓*_'}
 `.trim(), footer, user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '#owner' : '#menu', m)
     user.pc = new Date * 1
 }
