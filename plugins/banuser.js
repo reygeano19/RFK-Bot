2c53d9b2f7cb352c8ed9fcf803b7d50094cc2bea
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-03-02 04:15:10 +0700

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text }) => {
-    if (!text) throw 'Siapa yang mau di banned?'
+    if (!text) throw '_*Siape Yang Mo Koko Pangeran Entod❓*_'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
     if (!who) throw 'Tag salah satu lah'
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `berhasil banned`, m)
+    conn.reply(m.chat, `_*Asik Berhasil Koko Pangeran Entod😗*_`, m)
 }
 handler.help = ['banuser']
 handler.tags = ['owner']
