4436f40172a8052d055b8956cd2216e5599b5b3b
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-03-02 07:08:35 +0700

    Update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -12,6 +12,6 @@ let handler = async (m, { conn, text}) => {
 handler.help = ['ban']
 handler.tags = ['owner']
 handler.command = /^unban$/i
-handler.mods = true
+handler.rowner = true
 
 module.exports = handler
